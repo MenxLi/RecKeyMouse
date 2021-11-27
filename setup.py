123e2d5cd7179a46e7f2941c70f38765b1996338
monsoon-pro <mengxunli@whu.edu.cn>
2021-11-27 16:16:13 +0800

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="KeyMouseRecorder",
-    version="0.1.1",
+    version="0.1.2",
     author="Mengxun Li",
     author_email="mengxunli@whu.edu.cn",
     description="A screen action recorder",
